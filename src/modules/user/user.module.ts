950e5db679c01edbd140db7c6fe5f6373acb8bd3
Elmer Urrea <urrelmer@colpatria.com>
2020-07-06 08:10:47 -0500

    [REF] Refactor unused files and good practices

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { SharedModule } from '../../shared/shared.module';
 import { AuthModule } from '../auth/auth.module';
 import { RoleRepository } from '../role/role.repository';
 import { UserController } from './user.controller';
@@ -12,7 +11,6 @@ import { UserService } from './user.service';
   providers: [UserService],
   imports: [
     TypeOrmModule.forFeature([UserRepository, RoleRepository]),
-    SharedModule,
     AuthModule,
   ],
 })
